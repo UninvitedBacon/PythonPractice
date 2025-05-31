9aca9fd702731588e5eb8b519d81d111750470c1
UninvitedBacon <Davidhyman8@hotmail.com>
2025-05-31 10:06:40 -0700

    Co-authored-by: jooshhart <jooshhart@users.noreply.github.com>

---
+++ Hello copy 2.py
@@ -0,0 +1,17 @@
+
+
+import random
+aD20=list(range(1, 21))
+aD12=list(range(1, 13))
+aD10=list(range(1, 11))
+aD8=list(range(1, 9))
+aD6=list(range(1, 7))
+aD4=list(range(1, 5))
+
+
+diceroll = random.choice(aD4); {"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":11,"12":12,"13":13,"14":14,"15":15,"16":16,"17":17,"18":18,"19":19,"20":20}
+
+strength = 8 + diceroll
+print (strength)
+strength = 8 + diceroll
+print (strength)
---
+++ adventure.py
@@ -0,0 +1,46 @@
+
+
+import random
+randomoptions = ["true","false"]
+valid="false"
+while (valid=="false"):
+    option1 = input ("the cat is about to puch the vase off of the table. What will you do? \n(1) kill the cat. \n(2) stop the cat \n(3) catch the vace \n(4) do nothing\n")
+    if option1=="1":
+        option2_1 = print ("You sucsessfully shoot the cat before it gets to the vase")
+        catliving = "false"
+        vaseintack = "true"
+        valid = "true"
+    elif option1=="2":
+        option2_2 = print ("The cat scratches you, but you stop it from breaking the vace")
+        catliving = "true"
+        vaseintack = "true"
+        valid = "true"
+    elif option1=="3":
+        option1_3_sucsess = random.choice (randomoptions)
+        if (option1_3_sucsess == "yes"):
+            option2_3a = print ("you catch it in time, and the vase is fine")
+            catliving = "true"
+            vaseintack = "true"
+            valid = "true"
+        if (option1_3_sucsess=="no"):
+            option2_3b = print ("you do not catch the vase in time, and it shatters")
+            catliving = "true"
+            vaseintack = "flase"
+            valid = "true"
+    elif option1=="4":
+        option2_4 = print ("... The vase shatters")
+        catliving = "true"
+        vaseintack = "flase"
+        valid = "true"
+    else:
+        print ("just type a number",option1,"is invalid")
+
+valid="false"
+
+while (valid=="false"):
+    if (catliving=="true") and (vaseintack=="true"):
+        input ("")
+    if (catliving=="false") and (vaseintack=="true"):
+        input ("You realize that the cat's owner is coming soon. What will you do? \n(1) prepare for combat \n(2) appoligize \n(3) hide the cat \n(4) tell the owner the cat deserves it\n")
+    if (catliving=="true") and (vaseintack=="false"):
+        input ("")
